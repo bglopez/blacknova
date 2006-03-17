656c651715b9e16672a0559256f0b840fb110d10
iamsure <iamsure@61ace53f-96b9-4a07-a109-ee3f1b2c30ce>
2006-03-17 04:23:16 +0000

    Cleaning out old code in the repository

--- branches/bnt-0-4-0-proto/blacknova/sched_defenses.php
+++
@@ -1,9 +0,0 @@
-<?
-  if (preg_match("/sched_defenses.php/i", $PHP_SELF)) {
-      echo "You can not access this file directly!";
-      die();
-  }
-
-  $db->Execute("DELETE from $dbtables[sector_defence] where quantity <= 0");
-
-?>
